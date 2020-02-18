828179ce331c6c70552387a1b89a4a27e1575179
ge46ban <fenn@wzw.tum.de>
2020-02-18 14:40:02 +0100

    HISAT/POSIX fixed, builds index from GTF in temp folder.

--- hisat2/ENTRYPOINT.sh
+++ hisat2/ENTRYPOINT.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 second_attempt() {
-
-hisat2 -q  -x /docker_main/latest_human -U "$line"?.fastq -S /myvol1/hisat-output/test.sam --known-splicesite-infile /docker_main/splicesites.txt
+#Attempt Unpaired alignment
+hisat2 -q  -x /myvol1/hisat-output/temp/ -U "$line"?.fastq -S /myvol1/hisat-output/"${line##*/}".sam --known-splicesite-infile /myvol1/hisat-output/temp/splicesites.txt
 
 }
 
+#START here: Make a list of all fastq files
 find /myvol1/ -name "*fastq" -nowarn | sed s/.fastq// | sed 's/.$//' | sort | uniq >/myvol1/hisat-fastqlist
 
-mkdir /myvol1/hisat-output
+#Make output directories
+mkdir -p /myvol1/hisat-output/temp
 
+
+#Build Genome index
+cd /myvol1/hisat-output/temp/
+
+hisat2-build /docker_main/Homo_sapiens.GRCh38.dna.primary_assembly.fa simulated_splice_data_ && python /docker_main/hisat2-2.0.0-beta/extract_splice_sites.py $(find /myvol1/ -name "*.gtf") > splicesites.txt
+
+cd /myvol1/hisat-output/
+
+#Iterate list with paired end map command first
 while read -r line; do
 
+#First attempt: Paired end mapping
+hisat2 -q  -x /myvol1/hisat-output/temp/ -1 "$line"1.fastq -2 "$line"2.fastq -S /myvol1/hisat-output/"${line##*/}".sam --known-splicesite-infile /myvol1/hisat-output/temp/splicesites.txt
 
-
-#First attempt
-hisat2 -q  -x /docker_main/latest_human -1 "$line"1.fastq -2 "$line"2.fastq -S /myvol1/hisat-output/test.sam --known-splicesite-infile /docker_main/splicesites.txt
-
+#If paired end mapping fails, run unpaired mapping
 trap 'second_attempt $line' ERR
-
 done </myvol1/hisat-fastqlist
 
+#wait for all processes to end
 wait
+
+#cleaning up
 rm /myvol1/hisat-fastqlist
+rm -R /myvol1/hisat-output/temp
 echo "script is done"
