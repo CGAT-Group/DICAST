be8681068d844b8ed470f5fd71c5e4fc1588c5b2
ge46ban <fenn@wzw.tum.de>
2020-03-31 12:57:52 +0200

    demo done

--- dart/ENTRYPOINT.sh
+++ dart/ENTRYPOINT.sh
@@ -3,25 +3,25 @@ second_attempt() {
 #Unpaired mapping command
 #...tag outputs with this flag to name it per fastqfile 	"${line##*/}"
 #...address for all gtf files are 				$(find /myvol1/ -name "*.gtf")
-
-
+dart -i /myvol1/dartindex/ -f ${line}?.fastq  -o ${line}dart.sam
 
 }
-tool=
+tool=dart
 #START here: Make a list of fastq files
-find /myvol1/ -name "*fastq" -nowarn -maxdepth 1| sed s/.fastq// | sed 's/.$//' | sort | uniq >/myvol1/"$tool"-fastqlist
+find /myvol1/ -name "*fastq" -nowarn | sed s/.fastq// | sed 's/.$//' | sort | uniq >/myvol1/"$tool"-fastqlist
 
 #make output directories
 mkdir -p /myvol1/"$tool"-output/temp/
 
 #test filepaths for fasta and indexing
-#if ! test -f "/myvol1/Homo_sapiens.GRCh*.fa"; then echo "check the path for the Homo_sapiens.GRCh* fasta files: is it under <mounted folder>/Homo_sapiens.GRCh*.fa?"; exit; fi
-#if ! test -f "myvol1/star-output/temp/genomedir/SAindex"; then build_index; fi
+if ! test -f "/myvol1/Homo_sapiens.GRCh*.fa"; then echo "check the path for the Homo_sapiens.GRCh* fasta files: is it under <mounted folder>/Homo_sapiens.GRCh*.fa?"; exit; fi
+if ! test -d "myvol1/dartindex"; then build_index; fi
 
-
+build_index() {
 #Build Genome index
 # find gtf and da files with these  $(find /myvol1/ -name "*.fa")  $(find /myvol1/ -name "*.gtf")
-
+bwt_index /myvol1/Homo_sapiens.GRCh38.dna.primary_assembly.fa /myvol1/dartindex/
+}
 
 #Iterate list with paired end map command first
 while read -r line; do
@@ -29,6 +29,7 @@ while read -r line; do
 #First attempt: Paired end mapping
 #...tag outputs with this flag to name it per fastqfile         "${line##*/}"
 #...address for all gtf files are                               $(find /myvol1/ -name "*.gtf")
+dart -i /myvol1/dartindex/ -f ${line}1.fastq -f2 ${line}2.fastq -o ${line}dart.sam
 
 
 #If paired end mapping fails, run unpaired mapping.
