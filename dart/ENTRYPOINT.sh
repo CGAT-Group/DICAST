e041a4ac9483b208a5f84b8a37a3836813b503d2
ge46ban <fenn@wzw.tum.de>
2020-04-02 16:02:39 +0200

    dart-ENTRYPOINT

--- dart/ENTRYPOINT.sh
+++ dart/ENTRYPOINT.sh
@@ -11,7 +11,7 @@ build_index() {
 #Build Genome index
 # find gtf and da files with these  $(find /myvol1/ -name "*.fa")  $(find /myvol1/ -name "*.gtf")
 mkdir -p /myvol1/index/dartindex
-chmod 770 /myvol1/index/dartindex
+chmod 777 /myvol1/index/dartindex
 echo "compute index"
 bwt_index /myvol1/Homo_sapiens.GRCh38.dna.primary_assembly.fa /myvol1/index/dartindex/
 }
@@ -19,11 +19,11 @@ bwt_index /myvol1/Homo_sapiens.GRCh38.dna.primary_assembly.fa /myvol1/index/dart
 tool=dart
 #START here: Make a list of fastq files
 find /myvol1/ -name "*fastq" -nowarn | sed s/.fastq// | sed 's/.$//' | sort | uniq >/myvol1/"$tool"-fastqlist
-chmod 770 /myvol1/"$tool"-fastqlist
+chmod 777 /myvol1/"$tool"-fastqlist
 
 #make output directories
-mkdir -p /myvol1/"$tool"-output/temp/
-chmod 770 /myvol1/"$tool"-output/temp/
+mkdir -p /myvol1/"$tool"-output/
+chmod 777 /myvol1/"$tool"-output/
 
 #test filepaths for fasta and indexing
 if ! test -f "/myvol1/Homo_sapiens.GRCh38.dna.primary_assembly.fa"; 
@@ -41,7 +41,7 @@ while read -r line; do
 #First attempt: Paired end mapping
 #...tag outputs with this flag to name it per fastqfile         "${line##*/}"
 #...address for all gtf files are                               $(find /myvol1/ -name "*.gtf")
-dart -i /myvol1/index/dartindex/ -f ${line}1.fastq -f2 ${line}2.fastq -o ${line}dart.sam
+dart -i /myvol1/index/dartindex/ -f ${line}1.fastq -f2 ${line}2.fastq -o /myvol1/"$tool"-output/${line##*/}dart.sam
 
 
 #If paired end mapping fails, run unpaired mapping.
@@ -53,5 +53,4 @@ wait
 
 #cleaning up
 rm /myvol1/"$tool"-fastqlist
-rm -R /myvol1/"$tool"-output/temp
 echo "script is done"
