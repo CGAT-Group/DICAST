4bb820c7bb89df00bbf376fc568e8c9854bb0a30
Fanny Roessler <fanny.roessler@tum.de>
2020-06-19 17:41:31 +0200

    minimap with mapping specific func and config

---
+++ segemehl/ENTRYPOINT.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+### Needed Functions
+
+#Unpaired mapping command
+#...tag outputs with this flag to name it per fastqfile 	"${line##*/}"
+#...address for all gtf files are 				$(find /myvol1/ -name "*.gtf")
+second_attempt() {
+echo "paired mapping failed for ${line}. Try unpaired mapping."
+segemehl.x \
+	-d /myvol1/${fasta} \
+	-q ${line}.fastq \
+	-i /myvol1/index/${tool}-index/${fasta}.idx \
+	-t ${nthreads} \
+	-o /myvol1/output/"$tool"-output/${line##*/}${tool}.sam
+}
+
+build_index() {
+#Build Genome index
+# find gtf and da files with these  $(find /myvol1/ -name "*.fa")  $(find /myvol1/ -name "*.gtf")
+mkdir -p /myvol1/index/${tool}-index
+echo "compute index"
+segemehl.x -x /myvol1/index/${tool}-index/${fasta}.idx -d $(ls /myvol1/${fasta})
+chmod -R 777 /myvol1/index/${tool}-index/
+}
+
+#cleaning up
+cleaner() {
+rm /myvol1/tmp/"$tool"-fastqlist
+echo "script is done";exit;
+}
+
+#START here: Make a list of fastq files ############################################################################
+tool=segemehl
+nthreads=64
+fasta=Homo_sapiens.GRCh38.dna.primary_assembly.fa
+
+#test filepaths for fasta and indexing
+if ! test -f "/myvol1/${fasta}"; 
+	then echo "check the path for the ${fasta} fasta files: is it under <mounted folder>/${fasta}?";
+	cleaner;
+	exit; fi
+#if ! test -f "/myvol1/index/${tool}-index/${fasta}.idx"; then echo "would build index now"; fi
+if ! test -f "/myvol1/index/${tool}-index/${fasta}.idx"; then build_index; fi
+
+find /myvol1/ -name "*fastq" -nowarn -maxdepth 1| sed s/.fastq// | sed 's/.$//' | sort | uniq >/myvol1/tmp/"$tool"-fastqlist
+chmod 777 /myvol1/tmp/"$tool"-fastqlist
+
+#make output directories
+mkdir -p /myvol1/output/"$tool"-output/
+chmod -R 777 /myvol1/output/"$tool"-output/
+
+echo "compute ${tool} mapping..."
+#Iterate list with paired end map command first
+while read -r line; do
+
+#First attempt: Paired end mapping
+#...tag outputs with this flag to name it per fastqfile         "${line##*/}"
+#...address for all gtf files are                               $(find /myvol1/ -name "*.gtf")
+
+# -d 	--database [<file>] 	list of filename(s) of fasta database sequence(s)
+# -q 	--query 	filename of query sequences
+# -p 	--mate 	filename of paired end sequences
+# -i 	--index 	filename of database index
+#X -j 	--index2 	filename of second database index
+#X -x 	--generate 	generate db index and store to disk
+#X -y 	--generate2 	generate second db index and store to disk
+#X -G 	--readgroupfile 	filename to read @RG header (default:none)
+#X -g 	--readgroupid 	read group id (default:none)
+# -t 	--threads 	start threads (default:1)
+#X -f 	--fullname 	write full fastq/a name to SAM
+
+segemehl.x \
+	-d /myvol1/${fasta} \
+	-q ${line}1.fastq  \
+	-p ${line}2.fastq \
+	-i /myvol1/index/${tool}-index/${fasta}.idx \
+	-t ${nthreads} \
+	-o /myvol1/output/"$tool"-output/${line##*/}${tool}.sam
+
+#If paired end mapping fails, run unpaired mapping.
+trap 'second_attempt $line' ERR
+done </myvol1/tmp/"$tool"-fastqlist
+
+#wait for all processes to end
+wait
+#make output accessible
+chmod -R 777 /myvol1/output/"$tool"-output/
+cleaner
