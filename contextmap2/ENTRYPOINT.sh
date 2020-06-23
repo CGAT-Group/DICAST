6aa2ce4744f3fd2cf3df70d9c702ce9988fd555c
Fanny Roessler <fanny.roessler@tum.de>
2020-06-23 20:43:38 +0200

    contextmap entrypoint with func, config

--- contextmap2/ENTRYPOINT.sh
+++ contextmap2/ENTRYPOINT.sh
@@ -1,62 +1,95 @@
 #!/bin/bash
 
-##VARIABLES##
-fastapath=/myvol1/fasta/
-indexpath=/myvol1/contextmap2-output/indices/
-tool=contextmap2
-export fastapath
-export indexpath
+### Tool-specific variables ###
+tool=contextmap
 
-
-#WORKING DIRECTORY
-cd /myvol1
+# use confic and function file
+source /myvo11/config/mapping_config.sh
+source /myvol1/func/mapping_func.sh
 
 #To call contextmap as a command.
 PATH="$PATH":/home/biodocker/ContextMap_Source_v2.7.9/:/home/biodocker/jre1.8.0_241/
 
 
-###FUNCTIONS ###
+### Tool-specific functions ###
+
+# Unpaired mapping command: second attempt, used if paired end mapping failes
 second_attempt() {
-	#Unpaired mapping command
-	#...tag outputs with this flag to name it per fastqfile       "${line##*/}"
-	#...address for all gtf files are                              $(find /myvol1/ -name "*.gtf")
-	java -jar /home/biodocker/ContextMap_Source_v2.7.9/ContextMap_v2.7.9.jar mapper -reads ${line}?.fastq -aligner_name bowtie2 -o /myvol1/contextmap2-output -aligner_bin /home/biodocker/bin/    bowtie2 -indexer_bin /home/biodocker/bin/bowtie2-build -indices $(find ${indexpath} -name "*.bt2" | cut -f1 -d"." | tr '\n' ',' | sed s/,$//) -genome ${fastapath::-1}
+	# tag outputs with this flag to name it per fastqfile 	"${line##*/}"
+	echo "paired mapping failed for ${line}. Try unpaired mapping."
+	#-reads 		A comma separated list of file paths to reads in fasta/fastq/zip/gz format. A single path for single-end mapping and two paths (#1 mates and #2 mates) for paired-end mapping
+	#-aligner_name 	The following alignment tools are supported: "bwa", "bowtie1" or "bowtie2".
+	#-aligner_bin 	The absolute path to the executable of the chosen aligner tool.
+	#-indexer_bin 	The absolute path to the executable of the aligner's indexing tool (not needed for BWA).
+	#-indices 		A comma separated list of paths to basenames of indices, which can be used by the chosen aligner.
+	#-genome 		The path to a folder with reference sequences in fasta format (for each chromosome a separate file). It is important that the chromosome names contained in the index of the chosen unspliced alignment program (see 2. Obtaining ContextMap) are equal to the filenames of the files contained in the genome folder (for instance if the index contains a chromosome called "chr1", the genome folder must contain a file called "chr1.fa").
+	#-o 			The path to the output directory.
+	java -jar /home/biodocker/ContextMap_Source_v2.7.9/ContextMap_v2.7.9.jar \
+		mapper \
+		-reads ${line}?.fastq \
+		-aligner_name bowtie2 \
+		-o /$wd/$out/${line##*/}${tool}.sam \
+		-aligner_bin /home/biodocker/bin/\
+		-indexer_bin /home/biodocker/bin/bowtie2-build \
+		-indices /$wd/index/$tool-index/$index \
+		-genome $(ls /$wd/$inputdir/$fasta)
 }
 
 #Build Genome index
 build_index(){
-	mkdir "$indexpath"
-	for line in $(ls "$fastapath"); do bowtie2-build -f "$fastapath""$line" "$indexpath""$line" ; done
+	mkdir -p /$wd/index/$tool-index
+	echo "compute index ..."
+	for line in $(ls /$wd/$inputdir/$fasta); do bowtie2-build -f "/$wd/$inputdir/$fasta""$line" "/$wd/index/$tool-index""$fasta""$line" ; done
+	chmod -R 777 /myvol1/index/${tool}-index/
+	echo "Index is now saved under /$wd/index/$tool-index/$index"
 }
 
 
-#START HERE ###########
-#: Make a list of fastq files
-find /myvol1/ -maxdepth 1 -name "*fastq" -nowarn | sed s/.fastq// | sed 's/.$//' | sort | uniq >/myvol1/"$tool"-fastqlist
+### START here ############################################################################
+
+# test filepaths
+test_fasta
+
+# Build Genome index if not already available
+if ! test -f /$wd/index/$tool-index/$index.MT.1.bt2; then build_index; fi
+
+#make list of fastq files
+mk_fastqlist
 
 #make output directories
-mkdir -p /myvol1/"$tool"-output/
+mk_outdir
 
-#test filepaths for fasta and indexing
-if ! test -e ${fastapath}1.fa; then echo "check the path for the contextmap/bowtie fasta files: ${fastapath}1.fa"; exit; fi
-if ! test -f ${indexpath}MT.1.bt2; then build_index; fi
+### Start mapping ###
 
+echo "compute ${tool} mapping..."
 #Iterate list with paired end map command first
 while read -r line; do
+	#First attempt: Paired end mapping
+	#...tag outputs with this flag to name it per fastqfile         "${line##*/}"
 
-	echo "$line $indexpath $fastapath"
-#First attempt: Paired end mapping
+	#Parameters
+	#-reads 		A comma separated list of file paths to reads in fasta/fastq/zip/gz format. A single path for single-end mapping and two paths (#1 mates and #2 mates) for paired-end mapping
+	#-aligner_name 	The following alignment tools are supported: "bwa", "bowtie1" or "bowtie2".
+	#-aligner_bin 	The absolute path to the executable of the chosen aligner tool.
+	#-indexer_bin 	The absolute path to the executable of the aligner's indexing tool (not needed for BWA).
+	#-indices 		A comma separated list of paths to basenames of indices, which can be used by the chosen aligner.
+	#-genome 		The path to a folder with reference sequences in fasta format (for each chromosome a separate file). It is important that the chromosome names contained in the index of the chosen unspliced alignment program (see 2. Obtaining ContextMap) are equal to the filenames of the files contained in the genome folder (for instance if the index contains a chromosome called "chr1", the genome folder must contain a file called "chr1.fa").
+	#-o 			The path to the output directory.
+	
+	java -jar /home/biodocker/ContextMap_Source_v2.7.9/ContextMap_v2.7.9.jar \
+		mapper \
+		-reads ${line}1.fastq,${line}2.fastq \
+		-aligner_name bowtie2 \
+		-o /$wd/$out/${line##*/}${tool}.sam \
+		-aligner_bin /home/biodocker/bin/\
+		-indexer_bin /home/biodocker/bin/bowtie2-build \
+		-indices /$wd/index/$tool-index/$index \
+		-genome $(ls /$wd/$inputdir/$fasta)
 
-java -jar /home/biodocker/ContextMap_Source_v2.7.9/ContextMap_v2.7.9.jar mapper -reads ${line}1.fastq,${line}2.fastq -aligner_name bowtie2 -o /myvol1/contextmap2-output -aligner_bin /home/biodocker/bin/bowtie2 -indexer_bin /home/biodocker/bin/bowtie2-build -indices $(find ${indexpath} -name "*.bt2" | cut -f1 -d"." | tr '\n' ',' | sed s/,$//) -genome "${fastapath::-1}"
+	#If paired end mapping fails, run unpaired mapping.
+	trap 'second_attempt $line' ERR
+done </$wd/tmp/$tool-fastqlist
 
-#If paired end mapping fails, run unpaired mapping.
-trap 'second_attempt $line' ERR
-done </myvol1/"$tool"-fastqlist
-
-#wait for all processes to end
+# wait for all processes to end
 wait
-
-#cleaning up
-rm /myvol1/"$tool"-fastqlist
-rm -R /myvol1/"$tool"-output/temp 
-echo "script is done"
+cleaner
