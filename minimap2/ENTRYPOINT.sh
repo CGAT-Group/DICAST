5b5ecfcd74a7b92e91cc4d55840f671e69a8d929
Fanny Roessler <fanny.roessler@tum.de>
2020-06-25 19:30:39 +0200

    minimap EP indexdir etc

--- minimap2/ENTRYPOINT.sh
+++ minimap2/ENTRYPOINT.sh
@@ -4,7 +4,7 @@
 tool=minimap2
 
 # use confic and function file
-source /myvo11/config/mapping_config.sh
+source /myvol1/config/mapping_config.sh
 source /myvol1/func/mapping_func.sh
 
 ### Tool-specific functions ###
@@ -22,18 +22,18 @@ second_attempt() {
 	minimap2 \
 		-a \
 		-t $nthreads \
-		-o /$wd/$out/${line##*/}$tool.sam \
-		/$wd/index/$tool-index/$index \
+		-o $out/${line##*/}$tool.sam \
+		$indexdir/$index \
 		$line?.fastq
 }
 
 # make index directory and build index if index was not found
 build_index() {
-	mkdir -p /$wd/index/$tool-index
+	mkdir -p $indexdir
 	echo "compute index ..."
-	minimap2 -d /$wd/index/$tool-index/$index $(ls /$wd/$inputdir/$fasta)
-	chmod -R 777 /$wd/index/$tool-index
-	echo "Index is now saved under /$wd/index/$tool-index/$index"
+	minimap2 -d $indexdir/$index $(ls $inputdir/$fasta)
+	chmod -R 777 $indexdir
+	echo "Index is now saved under $indexdir/$index"
 }
 
 ### START here ############################################################################
@@ -42,7 +42,7 @@ build_index() {
 test_fasta
 	
 # Build Genome index if not already available
-if ! test -f /$wd/index/$tool-index/$index; then build_index; fi
+if $recompute_index; then build_index; else if ! test -f $indexdir/$index; then build_index; fi fi
 
 #make list of fastq files
 mk_fastqlist
@@ -68,14 +68,14 @@ while read -r line; do
 	minimap2 \
 		-a \
 		-t $nthreads \
-		-o /$wd/$out/${line##*/}${tool}.sam \
-		/$wd/index/$tool-index/$index \
+		-o $out/${line##*/}${tool}.sam \
+		$indexdir/$index \
 		${line}1.fastq \
 		${line}2.fastq
 
 	#If paired end mapping fails, run unpaired mapping.
 	trap 'second_attempt $line' ERR
-done </$wd/tmp/$tool-fastqlist
+done </tmp/$tool-fastqlist
 
 # wait for all processes to end
 wait
