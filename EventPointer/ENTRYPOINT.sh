46934f354fed21f243324c2d4e95c0bcae0b9459
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2020-06-29 18:40:42 +0200

    EventPointer entry workes with multiple BAMs now

--- EventPointer/ENTRYPOINT.sh
+++ EventPointer/ENTRYPOINT.sh
@@ -10,29 +10,36 @@ tool=eventpointer
 
 #test filepath for gtf and BAM+BAM-index
 test_gtf $wd/$gtf
-test_bam $wd/$bamfolder/$bamfile
+#test_bam $wd/$bamfolder/$bamfile
 
 
-#make output directory
+#make output directory and check BAM + BAM index
 mk_outdir $tool
+readbamfiles
 
 
-### Start AS event detection ###
+#### AS event detection mode ####
+for filename in $(cat $wd/$output/${tool:-unspecific}-output/bamlist)
+do
+	if [ $differential = 0 ]; then
+		makebamfromsam $filename
+        	sample_out=$(mk_sample_out $filename)
+	        echo Starting EventPointer for $filename in AS event detection mode...
+        	Rscript $wd/Rscripts/EventPointer.R --gtf $wd/$gtf --cores $ncores --out $sample_out --workdir $wd --bamfolder $bamfolder --bamfile $filename --differential $differential
+        	wait
+        	rm -f $wd/$output/${tool:-unspecific}-output/ASpli_binFeatures.log
+	fi
+done
+cleaner
 
-echo compute $tool AS event detection
 
-if [ $differential = 0 ]; then
-        echo Starting EventPointer in AS event detection mode...
-        Rscript $wd/Rscripts/EventPointer.R --gtf $wd/$gtf --cores $ncores --out $wd/$output/${tool:-unspecific}-output --workdir $wd --bamfolder $bamfolder --bamfile $bamfile --differential $differential
-	wait
-	cleaner
-fi
-
+#### DS mode ####
 if [ $differential = 1 ]; then
         echo Starting EventPointer in DS analysis mode...
-	combined=$(combine_case_control)
+	combined=$(combine_case_control) #combine case and control folder into single folder
         Rscript $wd/Rscripts/EventPointer.R --gtf $wd/$gtf --cores $ncores  --out $wd/$output/${tool:-unspecific}-output --workdir $wd --casefolder $casefolder --controlfolder $controlfolder --differential $differential --combined $combined
 	wait
+	rm -f $wd/$output/${tool:-unspecific}-output/ASpli_binFeatures.log
 	cleaner_diff
 fi
 
