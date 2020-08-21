a7bc56cad6b13f4d5b6e27b3c0f931c8cf0c72d2
Amit Fenn <fenn@wzw.tum.de>
2020-08-21 14:50:58 +0200

    Polishing the global run scripts

--- template/runscript.sh
+++ template/runscript.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 :> /nfs/proj/AS_dockers/images.txt
+echo "This script requires to be executed in the folder ./template/ of the CoMPASS project" ;echo -ne '.\r';sleep 1;echo -ne '..\r';sleep 1; echo -ne '...\r'; echo -ne '\n';sleep 1
+docker-compose build 
+echo $( docker images | grep proj | grep 0.01 | cut -d ' ' -f1 | cut -d '/' -f2|wc -l) images to run 
 for i in $( docker images | grep proj | grep 0.01 | cut -d ' ' -f1 | cut -d '/' -f2 ); do echo proj/${i}:0.01;done |tee /nfs/proj/AS_dockers/images.txt
-echo This script requires to be executed in the folder ./template/ of the CoMPASS project
+echo ------------
+echo " "
 read -r -p "update clusters? [Y/n] " input
 
 case $input in
@@ -11,6 +15,7 @@ updatecluster=1
  ;;
     [nN][oO]|[nN])
  echo "No"
+updatecluster=0
        ;;
     *)
  echo "Invalid input..."
