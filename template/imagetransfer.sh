2eb568db98d6bc522f459ad5b3bafe2b896f9405
Amit Fenn <fenn@wzw.tum.de>
2020-08-21 13:43:32 +0200

    added  before logging into ssh & the run context from runscript

--- template/imagetransfer.sh
+++ template/imagetransfer.sh
@@ -5,9 +5,9 @@ do docker save -o /nfs/proj/AS_dockers/images/${image}.tar "proj/${image}:0.01"
 done
 
 echo $HOSTNAME
-ssh afenn@bert 'for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo $HOSTNAME '
-ssh afenn@elmo 'for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file)  ; done ; echo $HOSTNAME '
-ssh afenn@ernie 'for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo $HOSTNAME '
-ssh afenn@grover 'for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo $HOSTNAME '
-ssh afenn@kermit 'for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo $HOSTNAME '
+echo logging onto Bert: ; ssh afenn@bert 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
+echo logging onto Elmo: ; ssh afenn@elmo 'echo $HOSTNAME: for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file)  ; done '
+echo logging onto Ernie: ; ssh afenn@ernie 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
+echo logging onto Grover: ; ssh afenn@grover 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
+echo logging onto Kermit: ; ssh afenn@kermit 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
 
--- template/runscript.sh
+++ template/runscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 :> /nfs/proj/AS_dockers/images.txt
 for i in $( docker images | grep proj | grep 0.01 | cut -d ' ' -f1 | cut -d '/' -f2 ); do echo proj/${i}:0.01;done |tee /nfs/proj/AS_dockers/images.txt
-
+echo This script requires to be executed in the folder ./template/ of the CoMPASS project
 read -r -p "update clusters? [Y/n] " input
 
 case $input in
@@ -20,6 +20,6 @@ esac
 
 if [ $updatecluster -eq 1 ]
 then
-/bin/bash /nfs/home/users/afenn/Dockers/dockers/template/imagetransfer.sh
+/bin/bash ./imagetransfer.sh
 fi
-sbatch /nfs/home/users/afenn/Dockers/dockers/template/slurm-docker-runner.sh
+sbatch ./slurm-docker-runner.sh
