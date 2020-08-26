9ee39ac38e44e2e10eab3d430b8a9f053b52f1a6
Amit Fenn <fenn@wzw.tum.de>
2020-08-26 18:52:09 +0200

    imagetransfer time and docker version commands

--- template/imagetransfer.sh
+++ template/imagetransfer.sh
@@ -7,7 +7,7 @@ echo -ne "\r\033[K All Docker images saved ^_^. Let's begin the export."
 echo " "
 
 echo $HOSTNAME
-echo logging $USER onto Bert: ; ssh ${USER}@bert 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
+echo logging $USER onto Bert: ; time -p ssh ${USER}@bert 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
 echo logging $USER onto Elmo: ; ssh ${USER}@elmo 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file)  ; done '
 echo logging $USER onto Ernie: ; ssh ${USER}@ernie 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
 echo logging $USER onto Grover: ; ssh ${USER}@grover 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
