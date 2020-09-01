5e29e96a7ef42f18f5aa95cf00bc9e23669d08ed
Amit Fenn <fenn@wzw.tum.de>
2020-09-01 15:19:35 +0200

    minor changes to runscripts

--- template/imagetransfer.sh
+++ template/imagetransfer.sh
@@ -7,9 +7,9 @@ echo -ne "\r\033[K All Docker images saved ^_^. Let's begin the export."
 echo " "
 
 echo $HOSTNAME
-echo logging $USER onto Bert: ; time -p ssh ${USER}@bert 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
-echo logging $USER onto Elmo: ; ssh ${USER}@elmo 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file)  ; done '
-echo logging $USER onto Ernie: ; ssh ${USER}@ernie 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
-echo logging $USER onto Grover: ; ssh ${USER}@grover 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
-echo logging $USER onto Kermit: ; ssh ${USER}@kermit 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done '
+echo logging $USER onto Bert: ; time -p ssh ${USER}@bert 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done  ; echo =========================================== '
+echo logging $USER onto Elmo: ; ssh ${USER}@elmo 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file)  ; done ; echo ============================================'
+echo logging $USER onto Ernie: ; ssh ${USER}@ernie 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo ============================================'
+echo logging $USER onto Grover: ; ssh ${USER}@grover 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo ============================================'
+echo logging $USER onto Kermit: ; ssh ${USER}@kermit 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done ; echo ============================================'
 
--- template/runscript.sh
+++ template/runscript.sh
@@ -18,18 +18,18 @@ echo "This script requires to be executed in the folder ./template/ of the CoMPA
 # Checking Run Context
 read -r -p "Is this the ./template/ folder of the CoMPASS project [Y/N]? " confirmdir
 case $confirmdir in
-    [yY][eE][sS]|[yY])
- echo "Yes"
+[yY][eE][sS]|[yY])
+echo "Yes"
 runscriptenv=0
- ;;
-    [nN][oO]|[nN])
- echo "No"
+;;
+[nN][oO]|[nN])
+echo "No"
 runscriptenv=1
-       ;;  
-    *)  
- echo "Invalid input..."
- exit 1
- ;;
+;;  
+*)  
+echo "Invalid input..."
+exit 1
+;;
 esac
 
 if [ $runscriptenv -eq 1 ] 
@@ -48,22 +48,20 @@ echo ------------
 echo "Enable dry run? [Y/N] "
 
 read -r -p " Enable dry run? [Y/N] " dryrunner
-
 case $dryrunner in
-    [yY][eE][sS]|[yY])
- echo "Yes"
+[yY][eE][sS]|[yY])
+echo "Yes"
 rundry=0
- ;;
-    [nN][oO]|[nN])
- echo "No"
+;;
+[nN][oO]|[nN])
+echo "No"
 rundry=1
-       ;;  
-    *)  
- echo "Invalid input..."
- exit 1
- ;;
+;;  
+*)  
+echo "Invalid input..."
+exit 1
+;;
 esac
-
 if [ $rundry -eq 0 ] 
 then
 popd
@@ -76,18 +74,18 @@ then
 read -r -p "Images will not be transfered, if the clusters are updated. Are the clusters updated? [Y/N] " input
 
 case $input in
-    [yY][eE][sS]|[yY])
- echo "Yes"
+[yY][eE][sS]|[yY])
+echo "Yes"
 updatecluster=0
- ;;
-    [nN][oO]|[nN])
- echo "No"
+;;
+[nN][oO]|[nN])
+echo "No"
 updatecluster=1
-       ;;
-    *)
- echo "Invalid input..."
- exit 1
- ;;
+;;
+*)
+echo "Invalid input..."
+exit 1
+;;
 esac
 if [ $updatecluster -eq 1 ]
 then
