b0defadb52253f449d822511d55f10e5001e907e
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2020-09-03 15:59:05 +0200

    first version of base config file

--- template/config.sh
+++ template/config.sh
@@ -1,11 +1,27 @@
-#set variables
-tool=cash
-wd=/myvol1 
-samfolder=/destaroutput 
-bamfolder=/destaroutput
-#bamCase= 
-#bamControl= 
-gtf=Homo_sapiens.GRCh38.99.gtf 
-fasta=Homo_sapiens.GRCh38.dna.primary_assembly.fa 
-out=output/$tool-output 
-nthreads=64 
+############################
+#     Basic Parameters     #
+############################
+
+ncores=6								#number of cores the tool will use
+workdir=/MOUNT								#name of the base directory inside the Docker
+outdir=$workdir/output/${tool:-unspecific}-output			#name of the output directory; will be named after the specific tool that was used
+read_length=76								#length of reads inside fastq files
+
+
+############################
+#     Input Parameters     #
+############################
+
+
+inputdir=$workdir/input							#base directory for all needed input files
+fastqdir=$workdir/$inputdir/fastq					#directory for fastqfiles
+bamdir=$workdir/$inputdir/bamdir					#directory for bamfiles
+samdir=$workdir/$inputdir/samdir					#directory for samfiles
+fasta=$workdir/$inputdir/fasta						#name of the genome reference file (fasta format)
+gtf=$workdir/$inputdir/gtf						#name of gtf reference file
+gff=$workdir/$inputdir/gff						#name of gff reference file
+indexdir=$workdir/$inputdir/index					#directory for STAR index
+indexname=$fasta							#name of the STAR index
+
+
+
