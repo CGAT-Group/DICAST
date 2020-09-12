1ec9bead218cd55c474dc4be3172c3c76e8f07c4
Amit Fenn <fenn@wzw.tum.de>
2020-09-12 13:07:08 +0200

    runscript now waits for user input after docker save

--- template/imagetransfer.sh
+++ template/imagetransfer.sh
@@ -4,7 +4,31 @@ for image in $(docker images | grep proj | grep 0.01 | cut -d ' ' -f1 | cut -d '
 do echo -ne "\r\033[K Saving the image for $image" && docker save -o /nfs/proj/AS_dockers/images/${image}.tar "proj/${image}:0.01" 
 done
 echo -ne "\r\033[K All Docker images saved ^_^. Let's begin the export."
-echo " "
+echo ------------
+
+read -r -p "Ready to export? [Y/N] " dryrunner
+case $dryrunner in
+[yY][eE][sS]|[yY])
+echo "Yes"
+rundry=1
+;;
+[nN][oO]|[nN])
+echo "No"
+rundry=0
+;;  
+*)  
+echo "Invalid input..."
+exit 1
+;;
+esac
+if [ $rundry -eq 0 ] 
+then
+popd
+exit
+fi
+
+
+
 
 echo $HOSTNAME
 echo logging $USER onto Bert: ; time -p ssh ${USER}@bert 'echo $HOSTNAME; for file in $(find /nfs/proj/AS_dockers/images/ -name '*.tar'); do echo $file $( docker load -i $file) ; done  ; echo =========================================== '
