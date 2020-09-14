ba919892fe1b8d96aafe723c14672ba34b5f3d06
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2020-09-14 12:41:35 +0200

    adapted ASpli to new config; removed unnecessary variables from asevent_config

--- ASpli/ENTRYPOINT.sh
+++ ASpli/ENTRYPOINT.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # use config and function file
-source /myvol1/config/asevent_config.sh
-source /myvol1/func/asevent_func.sh
 tool=aspli
+source template/config.sh
+source template/asevent_config.sh
+source template/asevent_func.sh
 
 
-test_gtf $wd/$gtf
-#test_bam $wd/$bamfolder/$bamfile
+#test input-files
+test_gtf $gtf
 
 #making outdir
-
 mk_outdir $tool
 
 #checking for SAM files and building BAM-files with index with them
 readsamfiles
-for filename in $(cat $wd/$output/${tool:-unspecific}-output/samlist)
+for filename in $(cat $outdir/samlist)
 do
         makebamfromsam $filename
 done
@@ -24,11 +24,11 @@ echo starting ASpli...
 
 if [ $differential = 0 ]; then
 	echo Starting ASpli in AS event detection mode...
-	Rscript $wd/Rscripts/ASpli.R --gtf $wd/$gtf --cores $ncores --readLength $read_length --out $wd/$output/${tool:-unspecific}-output --workdir $wd --bamfolder $bamfolder --differential $differential
+	Rscript /docker_main/ASpli.R --gtf $gtf --cores $ncores --readLength $read_length --out $outdir --workdir $workdir --bamfolder $bamdir --differential $differential
 fi
 if [ $differential = 1 ]; then
 	echo Starting Aspli in DS analysis mode...
-	Rscript $wd/Rscripts/ASpli.R --gtf $wd/$gtf --cores $ncores --readLength $read_length --out $wd/$output/${tool:-unspecific}-output --workdir $wd --casefolder $casefolder --controlfolder $controlfolder --differential $differential
+	Rscript /docker_main/ASpli.R --gtf $gtf --cores $ncores --readLength $read_length --out $outdir --workdir $workdir --casefolder $casefolder --controlfolder $controlfolder --differential $differential
 fi
 
 
--- template/asevent_config.sh
+++ template/asevent_config.sh
@@ -1,29 +1,12 @@
-###################
-#  basic values   #
-###################
-
-wd=/myvol1                                                              #name of mounted folder in docker container (don't change, unless you know what you are doing); all other path variables will start from here
-output=output                                                           #name of output folder (the tool name will be added to this name with "-$tool")
-ncores=60                                                               #number of cores to use
-
-
 ############################################
 #  values for standard AS event detection  #
 ############################################
 
-gtf=input_asevent/splicing_variants.gtf					#name of gtf-file; if not in $wd, give full path here
-#gtf=input_asevent/transcripts.gtf
-gff=input_asevent/splicing_variants.gff3				#name of gff3 file of reference
-bamfolder=input_asevent/bamfolder                                       #path of BAM-folder; always expecting BAM+BAI file
-fasta=input_asevent/genome.fa						#genome reference file (used by IRFinder, KisSplice and ASGAL)
 transcript=input_asevent/custom_transcripts.fasta			#fasta file for gene transcripts
-fastqfolder=input_asevent/fastqfolder					#path of fastq-folder; all files which want to be used have to end with .fastq
 
 #fastq files have to present as pairs; the next two fields contain the suffixes (including the file extension) all fastq-pairs present need to have (e.g. "_1.fastq" & "_2.fastq")
 fastqpair1suffix="_1.fastq"						#suffix for first file of fastq-pair 
 fastqpair2suffix="_2.fastq"						#suffix for second file of fastq-pair
-
-read_length=76								#length of mapped reads 
 
 
 #####################################
