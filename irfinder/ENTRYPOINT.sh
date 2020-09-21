6b55228458cf19b4caf2fc5d5fbe269736c25f66
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2020-09-21 20:05:49 +0200

    fixed IRFinder entry and dockerfile; now using gitlab version, not conda

--- irfinder/ENTRYPOINT.sh
+++ irfinder/ENTRYPOINT.sh
@@ -12,7 +12,6 @@ out=${wd}/${output}/${tool:-unspecific}-output  #local variable for readability
 ### build IRFinder custom reference ###
 #######################################
 
-
 #test for transcript.gtf and genome.fa
 gtf=$wd/$gtf
 fasta=$wd/$fasta
@@ -22,15 +21,14 @@ test_fasta $fasta
 
 #move both files into tmp folder
 echo linking annotation files into reference folder...
-mkdir -p $out/tmp
-chmod 777 $out/tmp
-ln -s $gtf $out/tmp/transcripts.gtf
-ln -s $fasta $out/tmp/genome.fa
+mkdir -p $out/index
+ln -sf $gtf $out/index/transcripts.gtf
+ln -sf $fasta $out/index/genome.fa
 
 
 #build reference
 echo building reference...
-IRFinder -m BuildRefProcess -r $out/tmp 
+IRFinder -m BuildRefProcess -r $out/index 
 wait
 echo reference built, moving on...
 
@@ -39,11 +37,17 @@ echo reference built, moving on...
 #### quantify IR ####
 #####################
 
-readfastqs $fastqfolder
-fastqs=$(cat $out/fastqlist)
+#readfastqs $fastqfolder
+#fastqs=$(cat $out/fastqlist)
 
 echo looking for intron retention events...
-IRFinder -r $out/tmp -d $out $fastqs 
+readbamfiles
+bams=$(cat $out/bamlist)
+for bam in $bams
+do
+       	IRFinder -m BAM -r $out/index -d $out $bam
+       	wait
+done
 
  
 cleaner
