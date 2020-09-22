15c42e19c00ab69729ac843c1ab29c6051976714
Amit Fenn <fenn@wzw.tum.de>
2020-09-22 14:40:32 +0200

    Star updated

--- Star/ENTRYPOINT.sh
+++ Star/ENTRYPOINT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tool=star
-
+star_version=2.7.5c
 source /myvol1/config/mapping_config.sh
 source /myvol1/func/mapping_func.sh
 
@@ -8,14 +8,14 @@ source /myvol1/func/mapping_func.sh
 #Build Genome index
 build_index() {
 	mkdir $indexdir; chmod 777 $indexdir
-	/docker_main/STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir $indexdir --genomeFastaFiles $inputdir/$fasta --runThreadN $nthreads --sjdbGTFfile $inputdir/$gtf -sjdbOverhang 100 --outFileNamePrefix $out/Star_mapped_${line##*/}
+	/docker_main/STAR-${star_version}/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir $indexdir --genomeFastaFiles $inputdir/$fasta --runThreadN $nthreads --sjdbGTFfile $inputdir/$gtf -sjdbOverhang 100 --outFileNamePrefix $out/Star_mapped_${line##*/}
 }
 
 #Unpaired mapping command
 second_attempt() {
 for line1 in $(ls ${line}*.fastq| sed s/.fastq// );
 do
-	/docker_main/STAR-2.7.3a/bin/Linux_x86_64/STAR --genomeDir $indexdir --outFileNamePrefix $out/Star_mapped_${line1##*/} --sjdbGTFfile $inputdir/$gtf  --twopassMode Basic --runThreadN $nthreads --outSAMstrandField intronMotif --outSAMattributes NH HI AS nM NM XS --readFilesIn ${line1}.fastq
+	/docker_main/STAR-${star_version}/bin/Linux_x86_64/STAR --genomeDir $indexdir --outFileNamePrefix $out/Star_mapped_${line1##*/} --sjdbGTFfile $inputdir/$gtf  --twopassMode Basic --runThreadN $nthreads --outSAMstrandField intronMotif --outSAMattributes NH HI AS nM NM XS --readFilesIn ${line1}.fastq
 done
 }
 
@@ -41,7 +41,7 @@ while read -r line; do
 
 	#First attempt: Paired end mapping
 echo mapping paired
-	/docker_main/STAR-2.7.3a/bin/Linux_x86_64/STAR --genomeDir  $indexdir --outFileNamePrefix $out/Star_mapped_${line##*/} --sjdbGTFfile $inputdir/$gtf  --twopassMode Basic --runThreadN $nthreads --outSAMstrandField intronMotif --outSAMattributes NH HI AS nM NM XS --readFilesIn ${line}1.fastq ${line}2.fastq
+	/docker_main/STAR-${star_version}/bin/Linux_x86_64/STAR --genomeDir  $indexdir --outFileNamePrefix $out/Star_mapped_${line##*/} --sjdbGTFfile $inputdir/$gtf  --twopassMode Basic --runThreadN $nthreads --outSAMstrandField intronMotif --outSAMattributes NH HI AS nM NM XS --readFilesIn ${line}1.fastq ${line}2.fastq
 
 	#If paired end mapping fails, run unpaired mapping.
 done < /tmp/$tool-fastqlist
