20561557a1d4fc824e9256d8dd3bf0be4cc93773
Amit <amitfenn@gmail.com>
2020-09-24 23:38:16 +0200

    Minor bug in docker local run script

--- template/docker-runner.sh
+++ template/docker-runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read image
-do;
+do
 {docker run -v "/nfs/proj/AS_dockers/:/myvol1" --user $(id -u):$(id -g) --rm $image ; echo This docker exited with status is: $?}> ./runlog-${image}.log
 echo $image $USER
 done < ./dockerrunlist 
