a1a36ed8dea0b3f80df33356f79889d676a78eee
Fanny Roessler <fanny.roessler@tum.de>
2020-09-28 18:35:10 +0200

    update general config

--- template/config.sh
+++ template/config.sh
@@ -2,26 +2,33 @@
 #     Basic Parameters     #
 ############################
 
-ncores=6								#number of cores the tool will use
+ncores=6										#number of cores the tool will use
 workdir=/MOUNT								#name of the base directory inside the Docker
 outdir=$workdir/output/${tool:-unspecific}-output			#name of the output directory; will be named after the specific tool that was used
 read_length=76								#length of reads inside fastq files
-
 
 ############################
 #     Input Parameters     #
 ############################
 
+fasta=Homo_sapiens.GRCh38.dna.primary_assembly.fa 		#name of the genome reference file (fasta format), directory=$fastadir
+gtf=splicing_variants.gtf										#name of gtf reference file, directory=$gtffile
+gff=splicing_variants.gff3									#name of gff reference file, directory=$gfffile
 
-inputdir=$workdir/input							#base directory for all needed input files
-fastqdir=$inputdir/fastq						#directory for fastqfiles
-bamdir=$inputdir/bamdir							#directory for bamfiles
-samdir=$inputdir/samdir							#directory for samfiles
-fasta=$inputdir/fasta							#name of the genome reference file (fasta format)
-gtf=$inputdir/gtf							#name of gtf reference file
-gff=$inputdir/gff							#name of gff reference file
-indexdir=$inputdir/index						#directory for STAR index
-indexname=$fasta							#name of the STAR index
+
+#############################
+#     Input Directories     #
+#############################
+
+
+inputdir=$workdir/inputdir	#base directory for all needed input files
+fastqdir=$inputdir/fastqdir	#directory for fastqfiles
+bamdir=$inputdir/bamdir		#directory for bamfiles
+samdir=$inputdir/samdir		#directory for samfiles
+fastadir=$inputdir				#directory for fastafile (might vary for specific tools -> see mapping or as-specific config file)
+gtfdir=$inputdir				#directory for gtffile
+gffdir=$inputdir				#directory for gfffile
+
 
 
 
