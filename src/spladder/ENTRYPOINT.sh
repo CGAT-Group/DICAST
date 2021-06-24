145c899602859e87f8750229bac7e51a353d69ba
Tim <ga92jup@mytum.de>
2021-06-24 12:50:04 +0200

    Spladder unification

--- src/spladder/ENTRYPOINT.sh
+++ src/spladder/ENTRYPOINT.sh
@@ -22,11 +22,37 @@ if [ $differential = 0 ]
 then
 	#list all .bam files in bamfolder with comma seperated
 	while read -r bamfile; do
-		bam_specific_out=$outdir/$(basename -- $bamfile)
+		bam_specific_out=$outdir/$(basename -s .bam $bamfile)_output
 		mkdir -p $bam_specific_out/
 		chmod -R 777 $bam_specific_out/
-		echo Starting spladder in event detection mode for $(basename -- $bamfile) ...
+		echo Starting spladder in event detection mode for $(basename -s .bam $bamfile) ...
 		spladder build -b $bamfile -o $bam_specific_out -a $gtf --parallel $ncores -n $read_length --output-txt-conf
+
+		echo "Running $tool unificiation..."
+
+		echo "Looking for $tool files in $bam_specific_out"
+		unified_outdir_name="${bam_specific_out}_${tool}_unified"
+		echo "Saving unified output to $unified_outdir_name"
+
+		# Unzip spladder output files and save to new tmp directory
+		uni_tmp="/tmp/unification_tmpdir"
+		mkdir $uni_tmp
+
+		for file in $bam_specific_out/*.confirmed.txt.gz ;
+		do
+			if [ -f "$file" ];
+			then
+				gunzip -c "$file" > $uni_tmp/$(basename -s .gz $file)
+			fi
+		done
+
+		if [ $combine_events = 0 ];
+		then
+			python3 /MOUNT/scripts/unified_output/output_transformer.py create -s $uni_tmp -out $unified_outdir_name -gtf $gtf
+		else
+			python3 /MOUNT/scripts/unified_output/output_transformer.py create -s $uni_tmp -out $unified_outdir_name -gtf $gtf -comb
+		fi
+		echo "Finished $tool unification for ${bam_specific_out}."
 	done < /tmp/controlbamlist
 	cleaner
 fi
